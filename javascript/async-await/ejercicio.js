7d994794d9773ec5026f56961da636070e0e51fd
Yulieth Paola Araque Sanchez <145613373+badpao02@users.noreply.github.com>
2023-12-14 02:23:19 +0000

    hola

--- javascript/async-await/ejercicio.js
+++ javascript/async-await/ejercicio.js
@@ -20,7 +20,7 @@ async function doTask(iterations) {
   (async () => {
     try {
       const result = await doTask(10);
-      console.log(result); // { error: false, value: [...] }
+      console.log(result); // { error: false, value: [...] }sdd
     } catch (error) {
       console.error(error); // { error: true, iter: ..., message: ... }
     }
